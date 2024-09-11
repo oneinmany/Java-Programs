eea7bae5ff3a5927c6f76648c2e9b02da3c472fb
purna chandu <sanneboyinapurnachandu@gmail.com>
2024-09-11 12:09:28 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+  public static void main(String[] args) {
+    String name = "purnachandu";
+    method(name);
+  
+
+  }
+  public static void method(String name){
+    System.out.println("my name is "+name+"!");
+  }
+}
