43b9502ebc67f7bf85e90d4a0638557d30fca014
purna chandu <sanneboyinapurnachandu@gmail.com>
2024-09-13 10:33:08 +0530

    Add files via upload

---
+++ Multipleobj/Main.java
@@ -0,0 +1,15 @@
+package Multipleobj;
+public class Main {
+  // Fields of the class
+  int X = 100;
+  String S = "Oneinmany";
+
+  public static void main(String[] args) {
+      // Create an instance of Main
+      Main myObj = new Main();
+      // Print fields to demonstrate usage
+      System.out.println(myObj.X);
+      System.out.println(myObj.S);
+  }
+}
+
---
+++ Multipleobj/Second.java
@@ -0,0 +1,18 @@
+package Multipleobj;
+public class Second {
+  public static void main(String[] args) {
+      // Create instances of Main
+      Main myObj1 = new Main();
+      Main myObj2 = new Main();
+
+      // Access and print fields from myObj1
+      System.out.println(myObj1.X);
+      System.out.println(myObj1.S);
+
+      // Optionally, print fields from myObj2 if needed
+      System.out.println(myObj2.X);
+      System.out.println(myObj2.S);
+  }
+}
+
+  
